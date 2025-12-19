2fc0b653271104ccb3eef9387f2c4532e9e7a7d1
StableCrimson <rhysethompson@gmail.com>
2025-12-19 16:42:53 -0600

    Add ruff

--- src/rom_utils.py
+++ src/rom_utils.py
@@ -1,153 +1,160 @@
 from typing import Literal, List
 from io import BytesIO
 
+
 def read_rom_bytes(offset: int, length: int = -1) -> bytes:
-  '''
-  Read a block of bytes from a ROM file.
-  Args:
-      offset (int): The offset in the ROM to start reading from.
-      length (int): The total length of data to read in bytes. If -1, read to the end of the file.
-  Returns:
-      bytes: The bytes read from the ROM.
-  '''
-  with open('baserom.gba', 'rb') as rom:
-    rom.seek(offset)
-    if length == -1:
-      return rom.read()
-    else:
-      return rom.read(length)
+    """
+    Read a block of bytes from a ROM file.
+    Args:
+        offset (int): The offset in the ROM to start reading from.
+        length (int): The total length of data to read in bytes. If -1, read to the end of the file.
+    Returns:
+        bytes: The bytes read from the ROM.
+    """
+    with open('baserom.gba', 'rb') as rom:
+        rom.seek(offset)
+        if length == -1:
+            return rom.read()
+        else:
+            return rom.read(length)
 
-def read_rom_table(offset: int, length: int, 
-                   element_size: int = 1) -> list[bytes]:
-  '''
-  Read a table of data from a ROM file.
-  Args:
-      offset (int): The offset in the ROM to start reading from.
-      length (int): The total length of data to read in bytes.
-      element_size (int): The size of each element in bytes.
-  Returns:
-      list[bytes]: The list of chunks read from the ROM.
-  '''
-  data = []
 
-  raw_data = BytesIO(read_rom_bytes(offset, length))
+def read_rom_table(offset: int, length: int, element_size: int = 1) -> list[bytes]:
+    """
+    Read a table of data from a ROM file.
+    Args:
+        offset (int): The offset in the ROM to start reading from.
+        length (int): The total length of data to read in bytes.
+        element_size (int): The size of each element in bytes.
+    Returns:
+        list[bytes]: The list of chunks read from the ROM.
+    """
+    data = []
 
-  for _ in range(length // element_size):
-    data.append(raw_data.read(element_size))
+    raw_data = BytesIO(read_rom_bytes(offset, length))
 
-  return data
+    for _ in range(length // element_size):
+        data.append(raw_data.read(element_size))
 
-def read_rom_int_table(offset: int, length: int, 
-                   element_size: int = 1, 
-                   endianness: Literal['little', 'big'] = 'little', 
-                   signed: bool = False) -> list[int]:
-  '''
-  Read a table of numbers from a ROM file.
-  Args:
-      offset (int): The offset in the ROM to start reading from.
-      length (int): The total length of data to read in bytes.
-      element_size (int): The size of each element in bytes.
-      endianness ('little', 'big'): The endianness of the data.
-      signed (bool): Whether the data is signed or unsigned.
-  Returns:
-      list[int]: The list of integers read from the ROM.
-  '''
-  data = []
+    return data
 
-  raw_data = read_rom_table(offset, length, element_size)
 
-  for entry in raw_data:
-    data.append(int.from_bytes(entry, endianness, signed=signed))
+def read_rom_int_table(
+    offset: int,
+    length: int,
+    element_size: int = 1,
+    endianness: Literal['little', 'big'] = 'little',
+    signed: bool = False,
+) -> list[int]:
+    """
+    Read a table of numbers from a ROM file.
+    Args:
+        offset (int): The offset in the ROM to start reading from.
+        length (int): The total length of data to read in bytes.
+        element_size (int): The size of each element in bytes.
+        endianness ('little', 'big'): The endianness of the data.
+        signed (bool): Whether the data is signed or unsigned.
+    Returns:
+        list[int]: The list of integers read from the ROM.
+    """
+    data = []
 
-  return data
+    raw_data = read_rom_table(offset, length, element_size)
+
+    for entry in raw_data:
+        data.append(int.from_bytes(entry, endianness, signed=signed))
+
+    return data
+
 
 def lz77_decompress(data: bytes | BytesIO) -> bytes:
-  '''
-  Decompress LZ77-compressed data from a GBA ROM.
-  Args:
-      data (bytes | BytesIO): The LZ77-compressed data.
-  Returns:
-      bytes: The decompressed data.
-  '''
+    """
+    Decompress LZ77-compressed data from a GBA ROM.
+    Args:
+        data (bytes | BytesIO): The LZ77-compressed data.
+    Returns:
+        bytes: The decompressed data.
+    """
 
-  if not isinstance(data, BytesIO):
-    data = BytesIO(data)
-  else:
-    data.seek(0)
+    if not isinstance(data, BytesIO):
+        data = BytesIO(data)
+    else:
+        data.seek(0)
 
-  # LZ77 header is a 4-byte word, where the lowest byte is the magic number (0x10),
-  # and the upper 3 bytes are the length of the _decompressed_ data.
-  header = int.from_bytes(data.read(4), 'little')
+    # LZ77 header is a 4-byte word, where the lowest byte is the magic number (0x10),
+    # and the upper 3 bytes are the length of the _decompressed_ data.
+    header = int.from_bytes(data.read(4), 'little')
 
-  magic = header & 0xFF
-  assert magic == 0x10, f'Invalid LZ77 magic! Expected: 0x10, Received 0x{magic:02x}'
+    magic = header & 0xFF
+    assert magic == 0x10, f'Invalid LZ77 magic! Expected: 0x10, Received 0x{magic:02x}'
 
-  decompressed_length = header >> 8
+    decompressed_length = header >> 8
 
-  decompressed_data = BytesIO()
-  bytes_written = 0
+    decompressed_data = BytesIO()
+    bytes_written = 0
 
-  while bytes_written < decompressed_length:
-    flags = int.from_bytes(data.read(1))
+    while bytes_written < decompressed_length:
+        flags = int.from_bytes(data.read(1))
 
-    # For each of the bits in the flag byte, from MSB to LSB
-    for i in range(8):
-      if bytes_written >= decompressed_length:
-        break
+        # For each of the bits in the flag byte, from MSB to LSB
+        for i in range(8):
+            if bytes_written >= decompressed_length:
+                break
 
-      # If the bit is 0, the next byte is a literal
-      # If the bit is 1, the next 2 bytes are a (displacement, length) pair
-      type = flags & (0x80 >> i)
+            # If the bit is 0, the next byte is a literal
+            # If the bit is 1, the next 2 bytes are a (displacement, length) pair
+            type = flags & (0x80 >> i)
 
-      # Next byte is literal
-      if type == 0:
-        value = int.from_bytes(data.read(1))
-        decompressed_data.write(bytes([value]))
-        bytes_written += 1
-        continue
+            # Next byte is literal
+            if type == 0:
+                value = int.from_bytes(data.read(1))
+                decompressed_data.write(bytes([value]))
+                bytes_written += 1
+                continue
 
-      # Next 2 bytes are (displacement, length) pair
-      # Displacement: 12 bits, Length: 4 bits
-      # Displacement is the distance back from the current position to copy from
-      # Length is the number of bytes to copy, minus 3 (so a length of 0 means copy 3 bytes)
-      value = int.from_bytes(data.read(2), 'little')
-      displacement = ((value & 0xF) << 8) | (value >> 8)
-      length = ((value >> 4) & 0xF)
+            # Next 2 bytes are (displacement, length) pair
+            # Displacement: 12 bits, Length: 4 bits
+            # Displacement is the distance back from the current position to copy from
+            # Length is the number of bytes to copy, minus 3 (so a length of 0 means copy 3 bytes)
+            value = int.from_bytes(data.read(2), 'little')
+            displacement = ((value & 0xF) << 8) | (value >> 8)
+            length = (value >> 4) & 0xF
 
-      for _ in range(length + 3):
-        byte = decompressed_data.getbuffer()[bytes_written - displacement - 1]
-        decompressed_data.write(bytes([byte]))
-        bytes_written += 1
+            for _ in range(length + 3):
+                byte = decompressed_data.getbuffer()[bytes_written - displacement - 1]
+                decompressed_data.write(bytes([byte]))
+                bytes_written += 1
 
-  return decompressed_data.getvalue()
+    return decompressed_data.getvalue()
+
 
 def split_blob(blob: bytes | BytesIO) -> List[bytes]:
-  '''
-  Split a blob of data into chunks based on a table of offsets.
-  The first 4 bytes of the blob indicate the number of entries (N).
-  The next N * 4 bytes are the offsets of each entry within the blob.
-  Args:
-      blob (bytes | BytesIO): The blob of data to split.
-  Returns:
-      list[bytes]: A list of byte chunks extracted from the blob.
-  '''
+    """
+    Split a blob of data into chunks based on a table of offsets.
+    The first 4 bytes of the blob indicate the number of entries (N).
+    The next N * 4 bytes are the offsets of each entry within the blob.
+    Args:
+        blob (bytes | BytesIO): The blob of data to split.
+    Returns:
+        list[bytes]: A list of byte chunks extracted from the blob.
+    """
 
-  if not isinstance(blob, BytesIO):
-    blob = BytesIO(blob)
-  else:
-    blob.seek(0)
+    if not isinstance(blob, BytesIO):
+        blob = BytesIO(blob)
+    else:
+        blob.seek(0)
 
-  num_chunks = int.from_bytes(blob.read(4), 'little')
-  offsets = []
-  chunks = []
+    num_chunks = int.from_bytes(blob.read(4), 'little')
+    offsets = []
+    chunks = []
 
-  for _ in range(num_chunks):
-    offsets.append(int.from_bytes(blob.read(4), 'little'))
+    for _ in range(num_chunks):
+        offsets.append(int.from_bytes(blob.read(4), 'little'))
 
-  for i in range(len(offsets)):
-    blob.seek(offsets[i])
-    end_offset = offsets[i + 1] if i + 1 < len(offsets) else len(blob.getbuffer())
-    chunk_size = end_offset - offsets[i]
-    chunks.append(blob.read(chunk_size))
+    for i in range(len(offsets)):
+        blob.seek(offsets[i])
+        end_offset = offsets[i + 1] if i + 1 < len(offsets) else len(blob.getbuffer())
+        chunk_size = end_offset - offsets[i]
+        chunks.append(blob.read(chunk_size))
 
-  return chunks
+    return chunks
--- src/structs.py
+++ src/structs.py
@@ -4,110 +4,122 @@ from dataclasses import dataclass
 import struct
 
 @dataclass
-class CType(ABC):    
-    '''
+class CType(ABC):
+    """
     Base class for C data types used in CStruct definitions.
     Attributes:
       size (int): Size of the data type in bytes.
       format (str): Struct format character for this data type.
       value (Any): The actual value of the data type.
-    '''
+    """
 
     size: int
-    '''Size of the data type in bytes.'''
+    """Size of the data type in bytes."""
 
     format: str
-    '''Struct format character for this data type.'''
+    """Struct format character for this data type."""
 
     value: Any
-    '''The actual value of the data type.'''
+    """The actual value of the data type."""
 
     def __init__(self, value: Any):
         self.value = value
 
     @classmethod
     def padding_needed(cls, offset: int) -> int:
-        '''
+        """
         Calculate the padding needed to align the current offset to this type's size.
         Args:
           offset (int): The current offset in bytes.
         Returns:
           pad (int): The number of padding bytes needed to achieve proper alignment for the data type.
-        '''
+        """
 
         pad = (cls.size - (offset % cls.size)) % cls.size
         return pad
-    
 
 
 class UInt8(CType):
     size = 1
     format = 'B'
 
+
 class UInt16(CType):
     size = 2
     format = 'H'
+
 
 class UInt32(CType):
     size = 4
     format = 'I'
 
+
 @dataclass
 class CStruct(ABC):
-    
     @classmethod
     def struct_format(cls) -> str:
-      '''
-      Generate the necessary struct format string for this CStruct subclass.
-      Returns:
-          fmt (str): The struct format string.
-      '''
+        """
+        Generate the necessary struct format string for this CStruct subclass.
+        Returns:
+            fmt (str): The struct format string.
+        """
 
-      fmt = '<' # GBA is little-endian
-      offset = 0
+        fmt = '<'  # GBA is little-endian
+        offset = 0
 
-      for _, type in get_type_hints(cls).items():
+        for _, type in get_type_hints(cls).items():
+            # This can be done "cleaner" with `filter`, but this gives us
+            # better type hints since Python can't infer the type on a filtered list.
+            if not issubclass(type, CType):
+                continue
 
-        # This can be done "cleaner" with `filter`, but this gives us
-        # better type hints since Python can't infer the type on a filtered list.
-        if not issubclass(type, CType):
-            continue
-        
-        padding = type.padding_needed(offset)
-        offset += padding
-        fmt += 'x' * padding
-        offset += type.size
-        fmt += type.format
+            padding = type.padding_needed(offset)
+            offset += padding
+            fmt += 'x' * padding
+            offset += type.size
+            fmt += type.format
 
-      return fmt
+        return fmt
 
     @classmethod
     def size(cls) -> int:
-      '''
-      Calculate the total size of this CStruct subclass in bytes.
-      Returns:
-          size (int): The total size in bytes.
-      '''
+        """
+        Calculate the total size of this CStruct subclass in bytes.
+        Returns:
+            size (int): The total size in bytes.
+        """
 
-      return struct.calcsize(cls.struct_format())
+        return struct.calcsize(cls.struct_format())
 
     @classmethod
     def from_bytes(cls, data: bytes):
-        assert len(data) >= cls.size(), f"Data must be at least {cls.size():02X} bytes long, got {len(data):02X} bytes."
-        
+        """
+        Create an instance of this CStruct subclass from a bytes.
+        Args:
+            data (bytes): The byte data to parse.
+        Returns:
+            instance (CStruct): An instance of the CStruct subclass.
+        """
+
+        assert len(data) >= cls.size(), (
+            f'Data must be at least {cls.size():02X} bytes long, got {len(data):02X} bytes.'
+        )
+
         unpacked_data = list(struct.unpack(cls.struct_format(), data))
         fields = []
 
         for _, type in get_type_hints(cls).items():
             if not issubclass(type, CType):
-              continue
+                continue
 
             fields.append(type(unpacked_data.pop(0)))
 
         return cls(*fields)
-    
-    def formatted_c(self, nesting: int = 1, annotated: bool = False, wide_hex: bool= False) -> str:
-        '''
+
+    def formatted_c(
+        self, nesting: int = 1, annotated: bool = False, wide_hex: bool = False
+    ) -> str:
+        """
         Generate a C-style struct representation of this CStruct instance.
         Args:
             nesting (int): The current nesting level for indentation.
@@ -115,7 +127,7 @@ class CStruct(ABC):
             wide_hex (bool): Whether to print hexidecimal values padded to 4 bytes.
         Returns:
             c_struct (str): The C-style struct representation.
-        '''
+        """
 
         lines = ['{']
         longest_field_name = max(len(name) for name in self.__dict__.keys())
@@ -123,18 +135,18 @@ class CStruct(ABC):
         for i, (name, value) in enumerate(self.__dict__.items()):
             if not isinstance(value, CType):
                 continue
-            
+
             annotation = ''
             if annotated:
-              annotation = f'/* {name.upper().center(longest_field_name)} */ '
+                annotation = f'/* {name.upper().center(longest_field_name)} */ '
 
             if wide_hex:
-              formatted_val = f'0x{value.value:04X}'
+                formatted_val = f'0x{value.value:04X}'
             else:
-              formatted_val = f'0x{value.value:02X}'
+                formatted_val = f'0x{value.value:02X}'
 
             # TODO: Allow nested structs
-            lines.append(f'{'\t' * nesting}{annotation}{formatted_val}')
+            lines.append(f'{"\t" * nesting}{annotation}{formatted_val}')
 
             if i < len(self.__dict__) - 1:
                 lines[-1] += ','
